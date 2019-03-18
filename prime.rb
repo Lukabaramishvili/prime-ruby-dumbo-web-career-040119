b0b638541f863e003064a051c3614d73f056bc4e
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-03-18 20:32:58 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,18 +2,5 @@
 
 def prime?(int)
   if int < 2 
-    return false 
-  else
-    return true 
-  end
-  
-  i = 2 
-  while i < int 
-    if int % i == 0 || int % 3 == 0 
-      return false 
-    else
-      return true
-    end
-    i += 1 
-  end
+
 end
